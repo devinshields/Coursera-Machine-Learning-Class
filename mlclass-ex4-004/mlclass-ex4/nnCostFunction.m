a3afbeb323ffa42e13553705cd1e94a456050e05
Devin Shields <devin.shields@gmail.com>
2013-12-20 15:24:51 -0500

    just realized I might need some of the inline variables I discarded :-(

--- mlclass-ex4-004/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-004/mlclass-ex4/nnCostFunction.m
@@ -35,17 +35,12 @@ Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
 
-
-
-% helpful logging
+% ====================== helpful logging ======================
 LOG('size(X) == %s', mat2str(size(X)))
 LOG('size(y) == %s', mat2str(size(y)))
 LOG('size(Theta1) == %s', mat2str(size(Theta1)))
 LOG('size(Theta2) == %s', mat2str(size(Theta2)))
 LOG('size(nn_params) == %s', mat2str(size(nn_params)))
-
-
-
 
 
 % ====================== YOUR CODE HERE ======================
@@ -58,18 +53,17 @@ LOG('size(nn_params) == %s', mat2str(size(nn_params)))
 %         computed in ex4.m
 %
 
-
-%  forward propogating example data through the network
-A2 = sigmoid([ones(m, 1) X ]           * Theta1');
+%  forward propogating features through the network
+A2 = sigmoid([ones(m, 1) X]           * Theta1');
 A3 = sigmoid([ones(size(A2, 1), 1) A2] * Theta2');
 
 
-% map the vector y into a boolean class membership matrix, Y.
+% map the vector y (in range(1, num_clases)) into a boolean membership matrix, Y.
 %     borrowed from: https://gist.github.com/denzilc/1360709
 Y = eye(num_labels)(y,:);
 
 
-% get element-wise errors over (i, k), then agg everything
+% get element-wise errors, then reduce-agg the whole matrix
 D1 = -Y    .* log(A3);
 D2 = (1-Y) .* log(1-A3);
 s0 = (1/m) * sum((D1 - D2)(:));
@@ -79,18 +73,9 @@ s0 = (1/m) * sum((D1 - D2)(:));
 reg_params = [Theta1(:,2:end)(:) ; Theta2(:,2:end)(:)];
 s1         = (lambda / 2 / m) * (reg_params' * reg_params);
 
-J          = s0 + s1;
+% sum to get the final penalty
+J = s0 + s1;
 
-
-
-%% map maximum value predictions/indicies into classifications
-%[x, p] = max(X2, [], 2);
-
-
-
-
-
-%keyboard
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -107,6 +92,30 @@ J          = s0 + s1;
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+
+% back propagate classifications errors for each example in the training set
+X0 = [ones(m, 1) X];
+
+for t = 1:m
+  xt  = X0(t, :);
+  yt  = Y(t, :);
+    
+  %  forward propogate
+  a1 = xt;
+  a2 = sigmoid(  a1    * Theta1');
+  a3 = sigmoid([1, a2] * Theta2');
+
+  % back propogate
+  d3 = a3 - yt;
+  d2 = Theta2' * d3 .* 
+  keyboard
+
+end
+
+
+
+
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
